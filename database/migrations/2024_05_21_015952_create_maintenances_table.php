dc6bc90328dd444ff13bd80225ec11c3497819c5
zainandhinf <zainandhif@gmail.com>
2024-05-21 09:05:57 +0700

    membuat model dan migration untuk maintenance

---
+++ database/migrations/2024_05_21_015952_create_maintenances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_maintenance');
+            $table->string('tanggal_maintenance');
+            $table->integer('biaya');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintenances');
+    }
+};
